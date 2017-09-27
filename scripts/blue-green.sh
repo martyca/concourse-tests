b7af02cb37769e39cb7e7e00df9c59c1a259a09e
Martin Caarels <martin@caarels.com>
2017-09-27 15:14:34 +0200

    blue green task done

--- scripts/blue-green.sh
+++ scripts/blue-green.sh
@@ -2,4 +2,6 @@
 # set
 
 cf login -a $api -u $username -p $password -o $organization -s $space
-cf apps
+cf map-route concourse-app-staged cfapps.io --hostname rws-concourse-test
+cf delete concourse-app -r -f
+cf rename concourse-app-staged concourse-app
