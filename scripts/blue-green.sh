b00572d2d610f3eb94d92091722d05f833892bc2
Martin Caarels <martin@caarels.com>
2017-09-27 15:38:58 +0200

    cleared up

--- scripts/blue-green.sh
+++ scripts/blue-green.sh
@@ -2,6 +2,8 @@
 # set
 
 cf login -a $api -u $username -p $password -o $organization -s $space
+cd ./this-git-repo/webapp/
+cf push
 cf map-route concourse-app-staged cfapps.io --hostname rws-concourse-test
 cf delete concourse-app -r -f
 cf rename concourse-app-staged concourse-app
