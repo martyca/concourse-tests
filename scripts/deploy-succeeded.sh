c1e27928a9b616df5cae2713360cd37be5467252
Martin Caarels <martin@caarels.com>
2017-09-28 11:37:33 +0200

    fixed env vars.

--- scripts/deploy-succeeded.sh
+++ scripts/deploy-succeeded.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # set
-# apk update
-# apk add curl
+apk update
+apk add curl
 set
 
-# json="{\"text\": \"Deployment Succeeded.\", \"username\": \"$slack-user\", \"channel\": \"$slack-channel\", \"icon_emoji\": \"$slack-icon\"}"
-# curl -s --data-urlencode "payload=$json" $slack-url
+json="{\"text\": \"Deployment Succeeded.\", \"username\": \"$slackuser\", \"channel\": \"$slackchannel\", \"icon_emoji\": \"$slackicon\"}"
+curl -s --data-urlencode "payload=$json" $slackurl
