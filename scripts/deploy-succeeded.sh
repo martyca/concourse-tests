ad8b35f6e55c7924b61b322a0e3811c8e553b19f
Martin Caarels <martin@caarels.com>
2017-09-28 11:18:19 +0200

    slack message added to success.

--- scripts/deploy-succeeded.sh
+++ scripts/deploy-succeeded.sh
@@ -3,3 +3,6 @@
 apk update
 apk add curl
 set
+
+json="{\"text\": \"Deployment Succeeded.\", \"username\": \"$slack-user\", \"channel\": \"$slack-channel\", \"icon_emoji\": \"$slack-icon\"}"
+curl -s --data-urlencode "payload=$json" $slack-url
