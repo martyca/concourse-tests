d14f73a9416d96d111a8b6b4532ee54d978ede5a
Martin Caarels <martin@caarels.com>
2017-09-28 11:26:21 +0200

    test

--- scripts/deploy-succeeded.sh
+++ scripts/deploy-succeeded.sh
@@ -4,5 +4,5 @@ apk update
 apk add curl
 set
 
-json="{\"text\": \"Deployment Succeeded.\", \"username\": \"$slack-user\", \"channel\": \"$slack-channel\", \"icon_emoji\": \"$slack-icon\"}"
-curl -s --data-urlencode "payload=$json" $slack-url
+#json="{\"text\": \"Deployment Succeeded.\", \"username\": \"$slack-user\", \"channel\": \"$slack-channel\", \"icon_emoji\": \"$slack-icon\"}"
+#curl -s --data-urlencode "payload=$json" $slack-url
