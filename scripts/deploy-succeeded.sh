3cb8ebb4994b324da31a1955fa41f156c758f270
Martin Caarels <martin@caarels.com>
2017-09-28 15:21:36 +0200

    added slack emoji's

--- scripts/deploy-succeeded.sh
+++ scripts/deploy-succeeded.sh
@@ -5,4 +5,4 @@ apk add curl
 set
 
 
-curl -s --data-urlencode "payload={\"text\": \"Deployment Succeeded.\", \"username\": \"$slackuser\", \"channel\": \"$slackchannel\", \"icon_emoji\": \"$slackicon\"}" $slackurl
+curl -s --data-urlencode "payload={\"text\": \":white_check_mark: Deployment Succeeded.\", \"username\": \"$slackuser\", \"channel\": \"$slackchannel\", \"icon_emoji\": \"$slackicon\"}" $slackurl
